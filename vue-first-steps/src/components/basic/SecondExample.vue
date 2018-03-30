bd57e02d9c8f408ba70ba16e1a80b6310584a8d4
Szabo Rihcard <ignocius@gmail.com>
2018-03-30 19:06:34 +0200

    clear index file and 2way databinding add

--- vue-first-steps/src/components/basic/SecondExample.vue
+++ vue-first-steps/src/components/basic/SecondExample.vue
@@ -5,65 +5,68 @@
     <p>{{ counter }} </p>
     <p v-on:mouseover="updateCoordinates">Cordinates: {{x}} / {{y}} - <span v-on:mouseover.stop="">DEAD SPOT</span></p>
     <input type="text" v-on:keyup.enter="alertMe">
-      <article>
-          <!-- 1) Show an alert when the Button gets clicked -->
-        <div>
-            <button @click="alertMe">Show Alert</button>
-        </div>
-        <!-- 2) Listen to the "keydown" event and store the value in a data property (hint: event.target.value gives you the value) -->
-        <div>
-            <input type="text" @keydown="onKeyDown">
-            <p>{{ value }}</p>
-        </div>
-        <!-- 3) Adjust the example from 2) to only fire if the "key down" is the ENTER key -->
-        <div>
-            <input type="text" @keydown.enter="onEnter">
-            <p>{{ valueE }}</p>
-        </div>
+    <article>
+        <!-- 1) Show an alert when the Button gets clicked -->
+      <div>
+          <button @click="alertMe">Show Alert</button>
+      </div>
+      <!-- 2) Listen to the "keydown" event and store the value in a data property (hint: event.target.value gives you the value) -->
+      <div>
+          <input type="text" @keydown="onKeyDown">
+          <p>{{ value }}</p>
+      </div>
+      <!-- 3) Adjust the example from 2) to only fire if the "key down" is the ENTER key -->
+      <div>
+          <input type="text" @keydown.enter="onEnter">
+          <p>{{ valueE }}</p>
+      </div>
     </article>
+    <section >
+      <input type="text" v-model="name" /> 
+      <!-- 2way databinding -->
+      <p>{{ name }}</p>
+    </section>
   </div>  
 </template>
 
 
 <script>
-
 export default {
-  name: 'SecondBasicExample',
-  data () {
+  name: "SecondBasicExample",
+  data() {
     return {
-      value: '',
-      valueE: '',
+      value: "",
+      valueE: "",
       counter: 0,
-      testArr: [1,2,3,4,5],
+      testArr: [1, 2, 3, 4, 5],
       x: 0,
       y: 0
-    }
+    };
   },
   methods: {
-    increase: ()=> {
+    increase: () => {
       console.log(this);
-      this.counter++
+      this.counter++;
       console.log(...this.testArr);
-    }, 
+    },
     increment: function(stpe) {
       console.log(this);
-      this.counter+= step
+      this.counter += step;
       console.log(...this.testArr);
-    
-    }, 
+    },
     updateCoordinates: function(ev) {
-      this.x = ev.clientX
-      this.y = ev.clientY
+      this.x = ev.clientX;
+      this.y = ev.clientY;
     },
     alertMe: function() {
-      alert('alert')
+      alert("alert");
     },
     onKeyDown: function() {
-      this.value = ev.target.value
+      this.value = ev.target.value;
     },
     onEnter: function() {
-      this.valueE = event.target.value
+      this.valueE = event.target.value;
     }
   }
-}
+};
 </script>
