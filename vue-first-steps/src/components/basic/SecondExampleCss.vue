dd05bca34546cb637295202dd171453e548ddfd3
HOME-Ricsi <szabo.ricsi@zip2000.hu>
2018-04-03 20:25:18 +0200

    first Exercise rdy

--- vue-first-steps/src/components/basic/SecondExampleCss.vue
+++ vue-first-steps/src/components/basic/SecondExampleCss.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>
+  <div class="flex">
     <section 
         class="demo" 
         @click="attachRed = ! attachRed"
         :class="divClasses"></section>
     <section class="demo" :class="{red: attachRed}"></section>
     <section class="demo" :class="[color, {red: attachRed}]"></section>
+    <section class="demo" :style="{backgroundColor: color}"></section>
+    <section class="demo" :style="myStyle"></section>
+    <section class="demo" :style="[myStyle, {height: width + 'px'}]"></section>
     <hr />
     <input type="text" v-model="color">
+    <input type="text" v-model="width">
+    <div id="exercise">
+        <!-- 1) Start the Effect with the Button. The Effect should alternate the "highlight" or "shrink" class on each new setInterval tick (attach respective class to the div with id "effect" below) -->
+        <div>
+            <button @click="startEffect">Start Effect</button>
+            <div id="effect" :class="highlight"></div>
+        </div>
+        <!-- 2) Create a couple of CSS classes and attach them via the array syntax
+        <div>I got no class :(</div>
+            3) Let the user enter a class (create some example classes) and attach it -->
+        <!-- <div>
+            <input type="text">
+            <div></div>
+        </div> -->
+        <!-- 4) Let the user enter a class and enter true/ false for another class (create some example classes) and attach the classes -->
+        <!-- <div>
+            <input type="text">
+            <input type="text">
+            <div></div>
+        </div> -->
+        <!-- 5) Repeat 3) but now with values for styles (instead of class names). Attach the respective styles.  -->
+        <!-- <div>
+            <input type="text">
+            <div></div>
+        </div> -->
+        <!-- 6) Create a simple progress bar with setInterval and style bindings. Start it by hitting the below button. -->
+        <!-- <div>
+            <button v-on:click="startProgress">Start Progress</button>
+            <div></div>
+        </div> --> 
+    </div>
   </div>  
 </template>
 
@@ -18,7 +52,10 @@ export default {
   data() {
     return {
      attachRed: false,
-     color: 'green'
+     color: 'green',
+     width: 100,
+     highlight: '',
+     colorPicker:  ['grren', 'red', 'blue']
     };
   },
   computed: {
@@ -27,12 +64,41 @@ export default {
               red: this.attachRed,
               blue: !this.attachRed
           }
+      },
+      myStyle: function() {
+          return {
+              backgroundColor: this.color,
+              width: this.width + 'px'
+          }
       }
-  }
-};
+  },
+  methods: {
+    startEffect: function() {
+        let vm  = this
+        setInterval(function(){
+            vm.highlight = vm.colorPicker[Math.round(Math.random() * 2, 2)]
+        }, 3000)
+    }
+ }
+}
+    
 </script>
 
 <style>
+#effect {
+    width: 20vw;
+    height: 20vh;
+    display: block;
+    border: 1px solid yellow;
+}
+.flex {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    width: 50%;
+}
 .demo {
     width: 50px;
     height: 50px;
